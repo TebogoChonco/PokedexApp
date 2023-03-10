bfaecf4aa90253c18c3caa540ad3513c692be223
Tebogochonco <115028731+Tebogochonco@users.noreply.github.com>
2023-03-10 06:37:38 +0200

    Corrections
    corrections made in class

--- pokedex-app/pokedex.js
+++ pokedex-app/pokedex.js
@@ -1,16 +1,14 @@
-function myBtn(display){
-         display.preventDefault()
-     "name" = document.getElementById("name").value;
-     "type" = document.getElementById("type").value;
-     "region" = document.getElementById("region").value;
+function logic() {
+
+    let name = document.getElementById("name").value;
+    let type = document.getElementById("type").value;
+    let region = document.getElementById("region").value;
 
     let pokemon = [];
-
-    pokemon.push("name", "type", "region");
+    pokemon.push(name, type, region);
 
     let liEl = document.createElement("li");
     let theSpan = document.createElement("span");
-
     let node1 = document.createTextNode(pokemon[0] + "  ");
     let node2 = document.createTextNode(pokemon[1] + "  ");
     let node3 = document.createTextNode(pokemon[2] + "  ");
@@ -19,24 +17,20 @@ function myBtn(display){
     theSpan.appendChild(node2);
     theSpan.appendChild(node3);
 
+    liEl.appendChild(theSpan);
+    document.getElementById("pokedexEntries").appendChild(liEl);
 
-liEl.appendChild(theSpan);
-document.getElementById("pokedexEntries").appendChild(liEl);
+    console.log(pokemon);
+    console.log(liEl);
 
-        console.log(pokemon);
-        console.log(liEl);
-
-        const previewButton =
+    const previewButton =
+    `
         <form action="http://google.com/search" target="_blank">
             <input name="q" hidden value="${name}"></input>
             <input type="submit"></input>
-        </form>
+        </form>`;
 
-
-        liEl.innerHTML (previewButton);
-
-document.getElementById("pokedexEntries").appendChild(liEl);
-
-        console.log(pokemon);
-        console.log(liEl);
+    document.getElementById("pokedexEntries").innerHTML+=previewButton;
+    console.log(pokemon);
+    console.log(liEl);
 }
